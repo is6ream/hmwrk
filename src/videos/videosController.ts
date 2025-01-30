ea91b54ab71c7b78e1daa12fcb078aa24af40f54
is6ream <dnlils10@icloud.com>
2025-01-30 08:07:52 +0500

    Создал роут

--- src/videos/videosController.ts
+++ src/videos/videosController.ts
@@ -1,2 +1,19 @@
+import { get } from 'http';
 import {Router, Request, Response} from 'express'
+import { db } from '../db/db'
+import { createVideoController } from './createVideoController'
+import { getVideosController } from './getVideosController'
 
+export const videoRouter = Router()
+
+const videosController = {
+    getVideosController: (req: Request, res: Response) => {
+        const videos = db.videos
+
+        res.status(200).json(videos)
+    },
+    createVideoController: (req: Request, res: Response) => createVideoController(req,res)
+}
+
+videoRouter.get('/', videosController.getVideosController)
+videoRouter.post('/', videosController.createVideoController)
