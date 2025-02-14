a4e98f068e83855f56b0184cdc52116fa6e46c34
is6ream <dnlils10@icloud.com>
2025-02-14 21:42:51 +0500

    Добавил отображения версии на верселе

--- dist/app.js
+++ dist/app.js
@@ -10,4 +10,7 @@ const routes_1 = require("./videos/routes");
 exports.app = (0, express_1.default)(); // создать приложение
 exports.app.use(express_1.default.json()); // создание свойств-объектов body и query во всех реквестах
 exports.app.use((0, cors_1.default)()); // разрешить любым фронтам делать запросы на наш бэк
+exports.app.get('/', (req, res) => {
+    res.status(200).json({ version: "1.0" });
+});
 exports.app.use('/hometask_01/api/videos/', routes_1.videosRouter);
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import { videosRouter } from './videos/routes'
 import { SETTINGS } from './settings'
+import { version } from 'os'
 
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
 app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
 
+
+app.get('/', (req, res) => {
+    res.status(200).json({ version: "1.0" })
+})
 app.use('/hometask_01/api/videos/', videosRouter);
 
 
